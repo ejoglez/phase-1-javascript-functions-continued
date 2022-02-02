6b1509d910bd4cb243a2c441b6ca3f82c5022fe6
Eric Gonzalez <ejoglez@gmail.com>
2022-02-02 09:57:30 -0600

    commit tree f1096e4d0efc178c2931cba132069f611ee80109

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun (activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = (task = 'go to the office') => {
+    return `This Monday, I will ${task}.`;
+}
+
+function wrapAdjective (flair = '*'){
+    const innerFun = function(adj = 'specail'){
+        return `You are ${flair}${adj}${flair}!`;
+    }
+    return innerFun
+}
